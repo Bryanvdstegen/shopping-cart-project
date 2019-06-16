385e20127429dae5ff35ea134a968320aaa4270a
Bryan Tran Lu <btl245@stern.nyu.edu>
2019-06-16 17:14:46 -0400

    Added name, and datetime

--- shopping_cart.py
+++ shopping_cart.py
@@ -61,11 +61,23 @@ while True:
 # INFO DISPLAY / OUTPUT
 #
 
+print("---------------------------------")
+print("GREEN FOODS GROCERY")
+print("WWW.GREEN-FOODS-GROCERY.COM")
+print("---------------------------------")
+
+import datetime
+today = datetime.date.today()
+print("CHECKOUT AT: " + " " + str(today)) #> '2017-07-02'
+
+print("---------------------------------")
+
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
     matching_product = matching_products[0] #
     total_price = total_price + matching_product["price"]
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+    print("... " + matching_product["name"] + " " + str(matching_product["price"]))
+
 
 print("TOTAL PRICE: " + str(total_price)) #TODO format in USD
 
